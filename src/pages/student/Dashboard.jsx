6f6c2ffe2438536448a31a192660af3fbabfadc3
inv-najil <najil.arfak@innovaturelabs.com>
2025-07-17 14:10:03 +0530

    feat:student dashboard shows thierown details

--- src/pages/student/Dashboard.jsx
+++ src/pages/student/Dashboard.jsx
@@ -1,3 +1,38 @@
+import { useState, useEffect } from "react";
+import API from "../../api/axios";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 export default function StudentDashboard() {
-  return <h3>Welcome Student</h3>;
+  const [student, setStudent] = useState([]);
+  useEffect(() => {
+    API.get("/students/")
+      .then((res) => {
+        setStudent(res.data.results);
+      })
+      .catch((err) => {
+        console.error("Error is fetching details", err);
+        setStudent([]);
+      })
+  }, [])
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell><strong>Name</strong></TableCell>
+            <TableCell><strong>Roll Number</strong></TableCell>
+            <TableCell><strong>Email</strong></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {student.map((student) => (
+            <TableRow key={student.id}>
+              <TableCell>{student.first_name} {student.last_name}</TableCell>
+              <TableCell>{student.roll_no}</TableCell>
+              <TableCell>{student.email}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
 }
